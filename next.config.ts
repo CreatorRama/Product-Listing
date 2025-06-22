6a1e55a0947baa08350b892acecd8a18e1623b96
Creator Rama <ap410485@gmail.com>
2025-06-22 05:36:36 +0530

    ty

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+   
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: 'http',
+        hostname: '**',
       },
-      // Add other image hosts if needed
     ],
-    // Optional optimization settings
+   
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
   },
