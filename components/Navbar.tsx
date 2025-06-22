59e484df01136ee9f0a8cb94c6d431bde7b5c34d
Creator Rama <ap410485@gmail.com>
2025-06-22 05:44:27 +0530

    added link to home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,12 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <h1 className="text-xl font-bold text-gray-800">AMRR TechSols</h1>
+              <Link
+                href="/"
+                className="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                <h1 className="text-xl font-bold text-gray-800">AMRR TechSols</h1>
+              </Link>
             </div>
           </div>
           <div className="flex items-center space-x-4">
